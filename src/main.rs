d5b0fb9b1df33ec69e37e0cc17a2eac1185f3fbc
Jared Moulton <jaredmoulton3@gmail.com>
2021-11-05 21:56:34 -0600

    Change executable name and add a limit arg and exit when suspended from interactive mode

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use regex::Regex;
 use skim::prelude::*;
 use std::io::Cursor;
 use std::io::Result;
-use std::process::{Command, Stdio};
+use std::process::Command;
 
 fn main() -> Result<()> {
-    let matches = App::new("grc-2")
+    let matches = App::new("github-repo-clone")
         .version("0.1.0")
         .author("Jared Moulton <jaredmoulton3@gmail.com>")
         .about("Scripts the usage of the github cli to make cloning slightly more convenient")
@@ -20,24 +20,23 @@ fn main() -> Result<()> {
                 .takes_value(true),
         )
         .arg(
-            Arg::new("repo")
-                .short('r')
-                .long("repo")
-                .about("The repo that you are looking for <not working>")
+            Arg::new("limit")
+                .short('l')
+                .long("limit")
+                .about("The number of repositories to list default=100")
                 .takes_value(true),
         )
-        .arg(Arg::new("git").multiple_values(true))
+        .arg(Arg::new("git args").multiple_values(true))
         .get_matches();
 
     let git_args = match matches.values_of("git") {
         Some(args) => args.collect::<Vec<_>>(),
-        None => Vec::new(), //collect::<Vec<_>>();
+        None => Vec::new(),
     };
 
     let arg_account = matches.value_of("account").unwrap_or("");
-    println!("{}", arg_account);
-    let arg_repo = matches.value_of("repo").unwrap_or("");
-    let mut command = "".to_string();
+    let arg_limit = matches.value_of("limit").unwrap_or("100");
+    let arg_limit = vec!["-L", arg_limit];
 
     let re_account_repo = Regex::new(r"[^\s]+").unwrap();
     let re_repo = Regex::new(r"/[^\s]+").unwrap();
@@ -52,21 +51,24 @@ fn main() -> Result<()> {
 
     // Commands
     let gh_output = Command::new("gh")
-        .args(&["repo", "list", "-l", "100"])
+        .args(&["repo", "list"])
         .arg(arg_account)
+        .args(arg_limit)
         .output()
         .expect("Couldn't execute gh binary with args");
 
     let item = item_reader.of_bufread(Cursor::new(gh_output.stdout));
-    let selected_item = Skim::run_with(&options, Some(item)).unwrap().selected_items;
+    let skim_output = Skim::run_with(&options, Some(item)).unwrap();
+    if skim_output.is_abort {
+        println!("No selection made");
+        std::process::exit(1);
+    }
+    let selected_item = skim_output.selected_items;
 
     for item in selected_item.iter() {
         let account_repo = &re_account_repo.captures(&item.output()).unwrap()[0].to_string();
         let repo = &re_repo.captures(account_repo).unwrap()[0].to_string()[1..];
-        if repo.is_empty() {
-            println!("No selection made");
-            std::process::exit(0);
-        }
+        if repo.is_empty() {}
         println!("cloning into {}", repo);
         Command::new("gh")
             .args(&["repo", "clone", account_repo, repo])
